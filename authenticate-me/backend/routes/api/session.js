87f6bc20fb2f5364c4e5ec1192f83d771ba875e3
Frances Huang <frances.y.huang@gmail.com>
2022-04-28 12:00:27 -0700

    finished seetting up backend

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -11,7 +11,7 @@ const validateLogin = [
 	check("credential")
 		.exists({ checkFalsy: true })
 		.notEmpty()
-		.withMessage("Please provide a valid email or username"),
+		.withMessage("Please provide a valid email or username."),
 	check("password")
 		.exists({ checkFalsy: true })
 		.withMessage("Please provide a password."),
