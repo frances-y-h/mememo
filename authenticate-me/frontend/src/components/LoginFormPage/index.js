1cf1f557bea21495dd2a2a888e0484f84fef184a
Frances Huang <frances.y.huang@gmail.com>
2022-04-28 18:21:04 -0700

    added login page

---
+++ authenticate-me/frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, Link } from "react-router-dom";
+import "./LoginForm.css";
+
+function LoginFormPage() {
+	const dispatch = useDispatch();
+	const sessionUser = useSelector((state) => state.session.user);
+	const [credential, setCredential] = useState("");
+	const [password, setPassword] = useState("");
+	const [errors, setErrors] = useState([]);
+
+	const errDiv = useRef();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErrors([]);
+		return dispatch(sessionActions.login({ credential, password })).catch(
+			async (res) => {
+				const data = await res.json();
+				if (data && data.errors) setErrors(data.errors);
+			}
+		);
+	};
+
+	useEffect(() => {
+		if (errors.length) {
+			errDiv.current.classList.remove("hidden");
+		} else {
+			errDiv.current.classList.add("hidden");
+		}
+	}, [errors]);
+
+	if (sessionUser) return <Redirect to="/" />;
+
+	return (
+		<div className="container-ctr">
+			<form className="form-control" onSubmit={handleSubmit}>
+				<div className="login-title">
+					<img className="logo" src="/images/logo.svg" alt="mememo" />
+					<h1 className="title">memeo</h1>
+					<div className="slogan">
+						A powerful memo app for hard working bees
+					</div>
+				</div>
+				<div className="form-group form-gap20">
+					<div ref={errDiv} className="error-list">
+						{errors.map((error, idx) => (
+							<li key={idx}>{error}</li>
+						))}
+					</div>
+					<div className="form-input-ctrl">
+						<i class="fa-solid fa-user"></i>
+						<input
+							type="text"
+							value={credential}
+							onChange={(e) => setCredential(e.target.value)}
+							required
+							placeholder="Username or Email"
+							className="input"
+						/>
+					</div>
+
+					<div className="form-input-ctrl">
+						<i class="fa-solid fa-lock"></i>
+						<input
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+							placeholder="Password"
+							className="input"
+						/>
+					</div>
+					<button className="btn" type="submit">
+						Login
+					</button>
+					<div className="form-group form-gap20">
+						<div className="form-link">Login as Demo User</div>
+						<div className="form-group">
+							<div className="form-link-label">Don't have an account?</div>
+							<Link to="/signup" className="form-link">
+								Sign Up
+							</Link>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default LoginFormPage;
--- authenticate-me/frontend/src/store/session.js
+++ authenticate-me/frontend/src/store/session.js
@@ -1,66 +1,51 @@
 import { csrfFetch } from "./csrf";
 
-// Actions
-const LOGIN = "session/LOGIN";
-const LOGOUT = "session/LOGOUT";
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => {
+	return {
+		type: SET_USER,
+		payload: user,
+	};
+};
+
+const removeUser = () => {
+	return {
+		type: REMOVE_USER,
+	};
+};
+
+export const login = (user) => async (dispatch) => {
+	const { credential, password } = user;
+	const response = await csrfFetch("/api/session", {
+		method: "POST",
+		body: JSON.stringify({
+			credential,
+			password,
+		}),
+	});
+	const data = await response.json();
+	dispatch(setUser(data.user));
+	return response;
+};
 
 const initialState = { user: null };
 
-// Reducer
-export default function sessionReducer(state = initialState, action) {
-	Object.freeze(state);
-
+const sessionReducer = (state = initialState, action) => {
 	let newState;
-
 	switch (action.type) {
-		case LOGIN:
-			newState = { ...state };
-			newState.user = action.user;
+		case SET_USER:
+			newState = Object.assign({}, state);
+			newState.user = action.payload;
 			return newState;
-		case LOGOUT:
-			newState = { ...state };
+		case REMOVE_USER:
+			newState = Object.assign({}, state);
 			newState.user = null;
 			return newState;
 		default:
 			return state;
 	}
-}
-
-//Action Creators
-export function login(user) {
-	return {
-		type: LOGIN,
-		user,
-	};
-}
-
-export function logout() {
-	return {
-		type: LOGOUT,
-	};
-}
-
-// Thunks
-// Payload must be in {"credential": "xxx", "password": "xxx"} format
-export const loginUser = (payload) => async (dispatch) => {
-	const response = await csrfFetch("/api/session", {
-		method: "POST",
-		body: JSON.stringify(payload),
-	});
-
-	const data = await response.json();
-	dispatch(login(data.user));
-	return response;
 };
 
-export const logoutUser = () => async (dispatch) => {
-	const response = await csrfFetch("/api/session", {
-		method: "DELETE",
-	});
-
-	if (response.ok) {
-		const message = await response.json().message;
-		dispatch(logout());
-		return message;
-	}
-};
+export default sessionReducer;
